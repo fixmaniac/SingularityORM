8472961782bb3e6db072021930702151c27e0dd0
fixmaniac <lucas.ligocki@gmail.com>
2016-05-19 23:50:17 +0200

    Foreign key

--- Singularity.ORM/Reader/SQLActionBase.cs
+++ Singularity.ORM/Reader/SQLActionBase.cs
@@ -20,38 +20,123 @@ namespace Singularity.ORM.Reader
     [DataObject]
     public abstract class SQLActionBase
     {
+        /// Basic properties
         protected MySqlConnection Connection { get; set; }
         protected SQLprovider Provider { get; set; }
-        private   IEnumerable<Type> entities;
+        protected List<Type> entities { get; set; }
 
+        /// <summary>
+        ///  Create SQL query 
+        /// </summary>
+        /// <returns></returns>
         internal SQLQuery CreateCommand()
         {
             return new SQLQuery(String.Empty);
         }
 
+        /// <summary>
+        ///  Return result 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <param name="cond"></param>
+        /// <returns></returns>
         protected T Result<T>(Type type, SQLCondition cond)
         {
-            return (T)get(typeof(T), type, cond);
+            try
+            {
+                return (T)get(typeof(T), type, cond);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
+
+        /// <summary>
+        /// Method that verify is field belongs to a specific table
+        /// </summary>
+        /// <param name="tablename"></param>
+        /// <param name="propertyname"></param>
+        /// <returns></returns>
         public bool IsProperly(string tablename, string propertyname)
         {
             Type type = null;
             return IsProperly(tablename, propertyname, ref type);
         }
+        /// <summary>
+        /// Method that verify is field belongs to a specific table
+        /// </summary>
+        /// <param name="tablename"></param>
+        /// <param name="propertyname"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public bool IsProperly(string tablename, string propertyname, ref Type type)
         {
-            entities = SQLprovider.GetAllEntities();
             Type entity = entities.Where(en => tablename == SQLprovider.getTableName(en)).FirstOrDefault();
-            if (entity != null) {
+            if (entity != null)
+            {
                 PropertyInfo pi = entity.GetProperty(propertyname.Trim(),
                     BindingFlags.Public | BindingFlags.Instance);
-                if (pi != null) {
+                if (pi != null)
+                {
                     type = pi.PropertyType;
                     return true;
                 }
+
             }
             return false;
         }
+
+        /// <summary>
+        ///  Method verified is property has a database relation (key)
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="type"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public bool IsForeignKey(Type source, string propertyName, ref Type type, ref string tableName)
+        {
+            var props = source.GetProperties().Where(prop
+                      => Attribute.IsDefined(prop, typeof(ForeignKeyAttribute)));
+            if (props.Count() == 0)
+                return false;
+            PropertyInfo pi = props.Where(p
+                      => p.Name == propertyName).FirstOrDefault();
+            if (pi == null)
+                return false;
+            ForeignKeyAttribute attr = (ForeignKeyAttribute)
+                                 pi.GetCustomAttribute(typeof(ForeignKeyAttribute));
+            tableName = attr.TableName.ToLower();
+            type = pi.PropertyType;
+            return true;
+        }
+
+        /// <summary>
+        /// SQL Conditions array
+        /// </summary>
+        string[] conds = new string[] 
+                                        { 
+                                            "AND", 
+                                            "OR" 
+                                        };
+
+        /// <summary>
+        /// Record conditions array
+        /// </summary>
+        string[] conds1 = new string[] 
+                                        { 
+                                            "=", 
+                                            "<>", 
+                                            "like", 
+                                            "not like", 
+                                            "is null", 
+                                            "is not null", 
+                                            " in" 
+                                        };
+
+
         /// <summary>
         /// Build join condition responsible for translation nested conditions
         /// </summary>
@@ -62,79 +147,78 @@ namespace Singularity.ORM.Reader
         /// <returns></returns>
         private string BuildJoinCondition(Type type, string table, string condition, out string newCondition)
         {
-
+            entities = new List<Type>();
             string query = SQLprovider.read;
-            string[] conds = new string[] 
-                                        { 
-                                            "AND", 
-                                            "OR" 
-                                        };
-            string[] conds1 = new string[] 
-                                        { 
-                                            "=", 
-                                            "<>", 
-                                            "like", 
-                                            "not like", 
-                                            "is null", 
-                                            "is not null", 
-                                            "in" 
-                                        };
             string[] arr = condition.Split(conds,
-                        StringSplitOptions.RemoveEmptyEntries);
+                           StringSplitOptions.RemoveEmptyEntries);
             List<string> arr2 = new List<string>();
             foreach (string s1 in arr)
             {
                 if (conds1.Any(c => s1.IndexOf(c) != -1))
                     arr2.Add(s1.Split(conds1, StringSplitOptions.None)[0]);
             }
-            List<Tuple<string, string>> joinedValues = new List<Tuple<string, string>>();
+
             List<string> joinedTables = new List<string>();
-            List<string> addedTables  = new List<string>();
+            List<string> addedTables = new List<string>();
+            List<Tuple<string, string>> joinedValues = new List<Tuple<string, string>>();
+
+
             addedTables.Add(table);
+
             foreach (string s in arr2)
             {
                 Type currentType = type;
+                entities.Add(type);
                 string currentTable = "";
-                if (s.Contains('.') && s.Split('.').Length > 1) {
+                if (s.Contains('.') && s.Split('.').Length > 1)
+                {
                     string[] fds = s.Split('.');
-                    for (int i = 0; i < fds.Length; ++i)  {
+                    for (int i = 0; i < fds.Length; ++i)
+                    {
                         currentTable = SQLprovider.getTableNameByField(currentType, fds[i], out currentType);
                         if (string.IsNullOrEmpty(currentTable)
                                 || currentTable.Equals(table))
                             continue;
-                        if (!joinedTables.Contains(currentTable)) {
+                        if (!joinedTables.Contains(currentTable))
+                        {
                             joinedTables.Add(currentTable);
-                       }
-                      joinedValues.Add(Tuple.Create(currentTable, s));
-                   }
+                        }
+                        joinedValues.Add(Tuple.Create(currentTable, s));
+                    }
                 }
             }
 
-            for (int i = 0; i < joinedTables.Count; ++i) {
+            for (int i = 0; i < joinedTables.Count; ++i)
+            {
                 string joinedTableName = joinedTables[i];
                 string _key = "";
                 string _table = "";
                 var tuple = joinedValues.Where(val => val.Item1 == joinedTableName).LastOrDefault();
                 var fields = tuple.Item2.Split('.');
                 Type _type = null;
+                string _tbl = "";
                 bool foundKey = false;
 
-                 foreach (string _field in fields)  {
-                    foreach (string _tablename in addedTables) {
-                        bool check = IsProperly(_tablename, _field, ref _type);
-                         if (check && _type != null
+                foreach (string _field in fields)
+                {
+                    foreach (string _tablename in addedTables)
+                    {
+                        bool isKey = IsForeignKey(type, _field, ref _type, ref _tbl);
+                        if (isKey && _type != null
                             && typeof(EntityProvider).IsAssignableFrom(_type)
-                            && SQLprovider.getTableName(_type) == joinedTableName) {
+                            && _tbl == joinedTableName)
+                        {
+                            entities.Add(_type);
                             _table = _tablename;
                             _key = _field;
                             addedTables.Add(joinedTableName);
                             foundKey = true;
-                              break;
+                            break;
                         }
-                   }
-                   if (foundKey)
-                          break;
-                   else  continue;
+                    }
+                    if (foundKey)
+                        break;
+                    else continue;
                 }
                 query = query.Replace("WHERE", String.Format("JOIN {0} ON {1}.{2} = {0}.Id",
                         joinedTableName,
@@ -142,7 +226,6 @@ namespace Singularity.ORM.Reader
                         _key));
                 query = query.Insert(query.IndexOf("{2}"), "WHERE ");
             }
-            
 
             joinedValues.ForEach(delegate(Tuple<string, string> tuple)
             {
@@ -206,14 +289,17 @@ namespace Singularity.ORM.Reader
                 object obj = null;
                 for (int i = 0; i < reader.FieldCount; ++i)
                 {
-                    if (i == 0 && obj == null) {
+                    if (i == 0 && obj == null)
+                    {
                         obj = Activator.CreateInstance(type);
                     }
-                    if (obj != null)  {
+                    if (obj != null)
+                    {
                         string field = !char.IsNumber(reader.GetName(i)[0])
                             ? reader.GetName(i) : string.Format("_{0}", reader.GetName(i));
                         object value = null;
-                        if (reader.GetValue(i).GetType() == typeof(DBNull))  {
+                        if (reader.GetValue(i).GetType() == typeof(DBNull))
+                        {
                             if (props[i] == typeof(int))
                                 value = 0;
                             else if (props[i] == typeof(string))
@@ -222,13 +308,14 @@ namespace Singularity.ORM.Reader
                         else
                         {
                             IEntityMapper EntityMapper = null;
-                            if (props[i] == typeof(bool))  {
+                            if (props[i] == typeof(bool))
+                            {
                                 EntityMapper = new BoolMapper
                                             (Provider, reader.GetValue(i));
                                 value = EntityMapper.Map();
                             }
-                            else if (props[i].IsEnum && reader.GetValue(i).GetType() == typeof(string))  {
-
+                            else if (props[i].IsEnum && reader.GetValue(i).GetType() == typeof(string))
+                            {
                                 var genericType = typeof(EnumMapper<>).MakeGenericType(props[i]);
                                 EntityMapper = (IEntityMapper)Activator.CreateInstance(genericType,
                                 new object[] {
@@ -238,15 +325,15 @@ namespace Singularity.ORM.Reader
                                 value = EntityMapper.Map();
                             }
                             else if (typeof(INotifyPropertyChanged).IsAssignableFrom(props[i])
-                                  && typeof(IBaseRecord).IsAssignableFrom(props[i])) {
-
+                                  && typeof(IBaseRecord).IsAssignableFrom(props[i]))
+                            {
                                 EntityMapper = new BusinessMapper
                                            (Provider, reader.GetValue(i), props[i]);
                                 value = EntityMapper.Map();
                             }
                             else if (props[i] == typeof(string) &&
-                                    reader.GetValue(i).GetType() == typeof(byte[])) {
-
+                                    reader.GetValue(i).GetType() == typeof(byte[]))
+                            {
                                 EntityMapper = new ByteStringMapper
                                                (Provider, reader.GetValue(i));
                                 value = EntityMapper.Map();
