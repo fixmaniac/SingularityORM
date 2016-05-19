947cf08c4d569d92ca237f36867164f5f772791e
fixmaniac <lucas.ligocki@gmail.com>
2016-05-19 23:53:23 +0200

    Foreign Key Attribute

---
+++ Singularity.ORM/Attributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Singularity.ORM
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class ForeignKeyAttribute : Attribute
+    {
+        public string TableName
+        {
+            get;
+            set;
+        }
+        public ForeignKeyAttribute(string tableName)
+        {
+            this.TableName = tableName;
+        }
+    }
+}
