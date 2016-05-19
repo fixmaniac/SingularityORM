434680f5bef8ce3720e4922ac2f85cc21e003b27
fixmaniac <lucas.ligocki@gmail.com>
2016-05-19 23:51:19 +0200

    Field State - Unchanged

---
+++ Singularity.ORM/EntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity.ORM
+{
+    public abstract class EntityRepository
+    {
+        public ISqlTransaction Transaction 
+        { 
+            get; 
+            set; 
+        }
+
+        public EntityRepository()
+        {
+        }
+
+        public static T GetInstance<T>(ISqlTransaction transaction) where T : EntityRepository, new()
+        {
+            T result = Activator.CreateInstance<T>();
+            result.Transaction = transaction;
+            return result;
+        }
+    }
+}
